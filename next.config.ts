cd67c07617bf267b3f4e811720ab0d4b8c8a67d4
Byoung Sik <art13233@naver.com>
2025-11-05 07:21:20 +0900

    fix: Next.js 16 Turbopack 異⑸룎 ?닿껐???꾪빐 webpack 紐⑤뱶 紐낆떆

--- next.config.ts
+++ next.config.ts
@@ -12,12 +12,6 @@ const nextConfig: NextConfig = {
       './node_modules/.prisma/client/libquery_engine-rhel-openssl-3.0.x.so.node',
     ],
   },
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.externals.push('@prisma/client');
-    }
-    return config;
-  },
 };
 
 export default nextConfig;
